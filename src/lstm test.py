2078c18a386d84d84982ae2577263c3ddfa50e00
Thomas Ager <tom@eygrr.com>
2018-02-20 19:53:37 +0000

    Added rudimentary hyper-parameter optimization for the Large Movie Review Dataset, based on the original example provided by Keras.

---
+++ src/lstm test.py
@@ -0,0 +1,238 @@
+'''Trains an LSTM model on the IMDB sentiment classification task.
+The dataset is actually too small for LSTM to be of any advantage
+compared to simpler, much faster methods such as TF-IDF + LogReg.
+# Notes
+- RNNs are tricky. Choice of batch size is important,
+choice of loss and optimizer is critical, etc.
+Some configurations won't converge.
+- LSTM loss decrease patterns during training can be quite different
+from what you see with CNNs/MLPs/etc.
+'''
+
+#CD PAPER PARAMETERS:
+#We implemented all models in Torch using default hyperparameters for weight initializa- tions. All models were optimized using Adam (Kingma & Ba, 2014) with the default learning rate of 0.001 using early stopping on the validation set. For the linear model, we used a bag of vectors model, where we sum pre-trained Glove vectors (Pennington et al., 2014) and add an additional lin- ear layer from the word embedding dimension, 300, to the number of classes, 2. We fine tuned both the word vectors and linear parameters.
+
+
+# LSTM Parameters
+# units: Positive integer, dimensionality of the output space.
+#  using a units number multiple of 32 may actually speed up a little the training (When you dealing with float32). So usually, people will do as such when designing their RNN.
+# Units = the amount of dimensions of the output space
+# activation: Activation function to use (see activations). If you pass None, no activation is applied (ie. "linear" activation:  a(x) = x). Default is tanh.
+# The activation function for updating the cell-state and when outputting
+# recurrent_activation: Activation function to use for the recurrent step (see activations) Default is hard_sigmoid.
+# The gating function for the three gates.
+# unit_forget_bias: Boolean. If True, add 1 to the bias of the forget gate at initialization. Setting it to true will also force  bias_initializer="zeros". This is recommended in Jozefowicz et al.
+# Bias on the forget gate as "The rationale is that the network shouldn't forget past information until it has learnt to forget it, and that it shouldn't bring in new info unless it has learnt that it is good to add new info to its internal state. While that seems sensible I have little idea whether it is useful in practice."
+# No regularizer or constraints used by defaulta
+# dropout: Float between 0 and 1. Fraction of the units to drop for the linear transformation of the inputs.
+# recurrent_dropout: Float between 0 and 1. Fraction of the units to drop for the linear transformation of the recurrent state.
+# return_sequences: Boolean. Whether to return the last output. in the output sequence, or the full sequence.
+# return_state: Boolean. Whether to return the last state in addition to the output.
+# go_backwards: Boolean (default False). If True, process the input sequence backwards and return the reversed sequence.
+# stateful: Boolean (default False). If True, the last state for each sample at index i in a batch will be used as initial state for the sample of index i in the following batch.
+# unroll: Boolean (default False). If True, the network will be unrolled, else a symbolic loop will be used. Unrolling can speed-up a RNN, although it tends to be more memory-intensive. Unrolling is only suitable for short sequences.
+
+
+from __future__ import print_function
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.datasets import imdb
+from keras import backend as K
+from itertools import product
+import numpy as np
+import random
+
+max_features_a = [5000] # Was 20,000 S
+maxlen_a = [300]  # cut texts after this number of words (among top max_features most common words) # L
+batch_size_a = [32] # M
+epochs_a = [64] #15,30,10 # L
+dropout_a = [0.3] # L
+recurrent_dropout_a = [0.05] # S
+embedding_size_a = [16] # S
+lstm_size_a = [32] # S
+learn_rate_a = [0.001] # S
+
+all_params = []
+
+all_params.append(max_features_a)
+all_params.append(maxlen_a)
+all_params.append(batch_size_a)
+all_params.append(epochs_a)
+all_params.append(dropout_a)
+all_params.append(recurrent_dropout_a)
+all_params.append(embedding_size_a)
+all_params.append(lstm_size_a)
+all_params.append(learn_rate_a)
+
+max_index_a = np.zeros(len(all_params), dtype="int")
+
+forget_bias = True
+dev = True
+overwrite = False
+
+print('Loading data...')
+
+# We need to load the IMDB dataset. We are constraining the dataset to the top 5,000 words. We also split the dataset into train (50%) and test (50%) sets.
+import os
+data_path = "../data/sentiment/lstm/"
+
+for i in range(len(all_params)):
+    max_features = all_params[0][max_index_a[0]]
+    maxlen = all_params[1][max_index_a[1]]
+    batch_size = all_params[2][max_index_a[2]]
+    epochs = all_params[3][max_index_a[3]]
+    dropout = all_params[4][max_index_a[4]]
+    recurrent_dropout = all_params[5][max_index_a[5]]
+    embedding_size = all_params[6][max_index_a[6]]
+    lstm_size = all_params[7][max_index_a[7]]
+    print(max_features, maxlen, batch_size, epochs, dropout, recurrent_dropout, embedding_size, lstm_size)
+    max_index = 0
+    max_acc = 0
+    for j in range(len(all_params[i])):
+        if i == 0:
+            max_features = all_params[0][j]
+        if i == 1:
+            maxlen = all_params[1][j]
+        if i == 2:
+            batch_size = all_params[2][j]
+        if i == 3:
+            epochs = all_params[3][j]
+        if i == 4:
+            dropout = all_params[4][j]
+        if i == 5:
+            recurrent_dropout = all_params[5][j]
+        if i == 6:
+            embedding_size = all_params[6][j]
+        if i == 7:
+            lstm_size = all_params[7][j]
+
+        file_name = "MF" + str(max_features) + " ML" + str(maxlen) + " BS" + str(batch_size) + " FB" + str(
+            forget_bias) + " DO" \
+                    + str(dropout) + " RDO" + str(recurrent_dropout) + " E" + str(epochs) + " ES" + str(
+            embedding_size) + "LS" + \
+                    str(lstm_size)
+
+        print (file_name)
+
+        if os.path.exists(data_path + "score/" + file_name + ".txt") and overwrite is False:
+            print("exists")
+            continue
+
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+
+        print("development data for hyperparameter tuning")
+        x_dev = x_train[int(len(x_train) * 0.8):]
+        y_dev = y_train[int(len(y_train) * 0.8):]
+        x_train = x_train[:int(len(x_train) * 0.8)]
+        y_train = y_train[:int(len(y_train) * 0.8)]
+
+        if dev:
+            y_test = y_dev
+            x_test = x_dev
+
+        # x_dev = x_train[:int(len(x_train)*0.2)]
+        # y_dev = y_train[:int(len(y_train)*0.2)]
+        # x_train = x_train[int(len(x_train)*0.2):]
+        # y_train = y_train[int(len(y_train)*0.2):]
+
+        print(len(x_train), 'train sequences')
+        print(len(x_test), 'test sequences')
+        # print(len(x_dev), 'dev train sequences')
+        # print(len(y_dev), 'dev test sequences')
+
+        # Next, we need to truncate and pad the input sequences so that they are all the same length for modeling. The model will learn the zero values carry no information so indeed the sequences are not the same length in terms of content, but same length vectors is required to perform the computation in Keras.
+        """"""
+        print('Pad sequences (samples x time)')
+        x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+        x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+        print('x_train shape:', x_train.shape)
+        print('x_test shape:', x_test.shape)
+
+
+        print('x_train shape:', x_train.shape)
+        print('x_test shape:', x_test.shape)
+
+        print('Build model...')
+
+        model = Sequential()
+
+        # The first layer is the Embedded layer that uses 32 length vectors to represent each word. The next layer is the LSTM layer with 100 memory units (smart neurons). Finally, because this is a classification problem we use a Dense output layer with a single neuron and a sigmoid activation function to make 0 or 1 predictions for the two classes (good and bad) in the problem.
+
+        model.add(Embedding(input_dim=max_features, output_dim=embedding_size))
+        # Input = size of vocab, output = dimension of dense embedding
+
+        model.add(
+            LSTM(units=lstm_size, recurrent_activation="hard_sigmoid", unit_forget_bias=forget_bias, activation="tanh",
+                 dropout=dropout, recurrent_dropout=recurrent_dropout, kernel_initializer="glorot_uniform"))
+
+        model.add(Dense(1, activation='sigmoid'))
+
+        # Because it is a binary classification problem, log loss is used as the loss function (binary_crossentropy in Keras). The efficient ADAM optimization algorithm is used. The model is          fit for only 2 epochs because it quickly overfits the problem. A large batch size of 64 reviews is used to space out weight updates.
+
+        # try using different optimizers and different optimizer configs
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        print('Train...')
+        model.fit(x_train, y_train,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  validation_data=(x_test, y_test))
+
+        output = model.predict(x_train)
+
+        inp = model.input  # input placeholder
+        outputs = [layer.output for layer in model.layers]  # all layer outputs
+        functor = K.function([inp] + [K.learning_phase()], outputs)  # evaluation function
+        # Note: To simulate Dropout use learning_phase as 1. in layer_outs otherwise use 0.
+
+        # Testing
+        test = x_train
+        layer_outs = functor([test, 1.])
+        print(layer_outs)
+
+        score, acc = model.evaluate(x_test, y_test,
+                                    batch_size=batch_size)
+        print('Test score:', score)
+        print('Test accuracy:', acc)
+
+
+
+        np.savetxt(data_path + "score/" + file_name + " acc" +".txt", [acc])
+        np.savetxt(data_path + "score/" + file_name + " score" + ".txt", [score])
+
+        model.save(data_path + "model/" + file_name + ".txt")
+
+        np.save(data_path + "vectors/" + file_name + " L" + str(1) + ".txt", layer_outs[1])
+
+        # Do the thing to get the acc
+        if acc > max_acc:
+            max_index = j
+            max_acc = acc
+    print("max option for param", i, "is", max_index, "with", max_acc)
+    max_index_a[i] = max_index
+    np.savetxt(data_path + "score/" + "max vals" + str(i) + " acc" + ".txt", max_index_a)
+    #np.savetxt(data_path + "score/" + "all params" + str(i) +  " acc" + ".txt", all_params)
+
+# Generate parameter list
+params = []
+"""
+for max_features in max_features_a:
+    for maxlen in maxlen_a:
+        for batch_size in batch_size_a:
+            for forget_bias in forget_bias_a:
+                for epochs in epochs_a:
+                    for dropout in dropout_a:
+                        for recurrent_dropout in recurrent_dropout_a:
+                            for embedding_size in embedding_size_a:
+                                for lstm_size in lstm_size_a:
+"""
+
+
+
+
